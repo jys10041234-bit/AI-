891856a5ab7f69adc9cb6f3d46b6338c4f9fd373
jys <jys10041234@gmail.com>
2026-03-16 17:23:30 +0900

    Update LAB2-10.py

--- LAB2-10.py
+++ LAB2-10.py
@@ -1,10 +1,10 @@
-name=input('이름을 입력하세요 :')
-print(name, '님이 입장하셨습니다.')
+name=input('이름을 입력하세요 :') 
+print(name, '님이 입장하셨습니다.') 
 
-m=int(input('숫자 m을 입력하세요:'))
-n=int(input('숫자 n을 입력하세요:'))
+m=int(input('숫자 m을 입력하세요:')) 
+n=int(input('숫자 n을 입력하세요:')) 
 print('m+n=',m+n)
 print('m-n=',m-n)
 
-radius=int(input('원의 반지름을 입력하세요: '))
-print(3.14*radius*radius, '원의 면적입니다.')
+radius=int(input('원의 반지름을 입력하세요: ')) #5입력
+print(3.14*radius*radius, '원의 면적입니다.') #78.5 원의 면적입니다.
