afcecf3320d4fadfbed4c25cda256e7f0a7a799e
jys <jys10041234@gmail.com>
2026-03-16 17:24:10 +0900

    Update print statements for variable values
    Refactor print statements to display variable values instead of variable names.

--- LAB2-4.py
+++ LAB2-4.py
@@ -1,21 +1,21 @@
 name='정윤성'
-print('나의 이름은 :', name)
+print('나의 이름은 :', name) # 정윤성
 age=22
-print('나의 나이는 :', age)
+print('나의 나이는 :', age) #22
 height=176
-print('나의 키는', height, 'cm 입니다.')
+print('나의 키는', height, 'cm 입니다.') #나의 키는 176cm 입니다.
 sum = 10+20
-print('10+20=', sum)
+print('10+20=', sum) # 10+20=30
 mult = 10*20
-print('10*20=',mult)
+print('10*20=',mult) # 10*20=200
 
 name='정윤성'
-print('나의 이름은 :', 'name')
+print('나의 이름은 :', 'name') # name
 age='22'
-print('나의 나이는 :','age')
+print('나의 나이는 :','age') # age
 height='176'
-print('나의 키는', 'height', 'cm 입니다.')
+print('나의 키는', 'height', 'cm 입니다.') # 나의 키는 height cm 입니다.
 sum = 10+20
-print('10+20=', 'sum')
+print('10+20=', 'sum') # 10+20=sum
 mult = 10*20
-print('10*20=','mult')
+print('10*20=','mult') # 10*20=mult
